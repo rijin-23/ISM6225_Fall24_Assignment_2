908f792ad0ce8cdae08974cd36a0ab976b774ef2
Rijin Thomas <rijin232@gmail.com>
2024-10-19 17:15:22 -0400

    Added comments to the program

--- ISM6225_Spring_2024_Assignment_2/Program.cs
+++ ISM6225_Spring_2024_Assignment_2/Program.cs
@@ -60,6 +60,18 @@ namespace Assignment_2
         // Question 1: Find Missing Numbers in Array
         public static IList<int> FindMissingNumbers(int[] nums)
         {
+            /* 
+            --LOGIC
+            
+            Traverse the elements in the array and if the element is not present
+            in an intially empty dictionary, the element will be added as a key to the dictionary
+
+            Once all the elements are traversed in the input array a loop starting from 1 to n 
+            will check if any number is missing from the Dictionary. The missing numbers will
+            then be appended to a list and at the end of the program, this list is returned. 
+
+            Time Complexity: O(n)
+            */
             try
             {
 				List<int> newList = new List<int>();
@@ -85,6 +97,18 @@ namespace Assignment_2
         // Question 2: Sort Array by Parity
         public static int[] SortArrayByParity(int[] nums)
         {
+            /* 
+            --LOGIC
+
+            Two pointer is used to sort the elements in place hence the space complexity is O(1).
+            Two indices i and j will be used to iterate through the array. If the element is an
+            odd number, then the i will go to the next element, keeping j the same. If the element in the
+            array is an even number but i and j are same, then that element is skipped, meaning 
+            it's already sorted. If the element is an even number but i and j are not the same, then
+            the elements at the position i and j are swapped.
+
+            Time Complexity: O(n)
+            */
             try
             {
 				int i = 0;
@@ -119,6 +143,17 @@ namespace Assignment_2
         // Question 3: Two Sum
         public static int[] TwoSum(int[] nums, int target)
         {
+            /*
+            --LOGIC
+
+            Iterate through the elements of the array and check if the difference between the
+            target and the current element exists in the declared dictionary. If the difference
+            doesn't exist, <k,v> = <element, index> is added to the dictionary. If the difference
+            does exist in the dictionary, index(v) for difference(k) is appended to a list and then
+            the current index of the element is also added to the list which is then returned.
+
+            Time Complexity: O(n)
+            */
             try
             {	
 				List<int> resList = new List<int>();
@@ -128,7 +163,7 @@ namespace Assignment_2
 						newDict.Add(nums[i],i);
 					}
 					else{
-						Console.WriteLine(newDict[target-nums[i]]);
+						//Console.WriteLine(newDict[target-nums[i]]);
 						resList.Add(newDict[target-nums[i]]);
 						resList.Add(i);
 						return resList.ToArray();
@@ -145,6 +180,18 @@ namespace Assignment_2
         // Question 4: Find Maximum Product of Three Numbers
         public static int MaximumProduct(int[] nums)
         {
+            /*
+            --LOGIC
+
+            So for a 3 number to yield a maximum product, we need to find out the lowest, the second
+            lowest, the highest, the second highest and the third highest. The lowest and the second
+            lowest numbers could be negative and the product of two negatives is always positive. Hence,
+            we need to check the product of lowest, second lowest and the highest and compare it with
+            the product of highest, second highest and third highest. Whichever produces the higher result
+            will then be returned.
+
+            Time Complexity: O(n)
+            */
             try
             {
 				int lowest = int.MaxValue;
@@ -196,6 +243,15 @@ namespace Assignment_2
         // Question 5: Decimal to Binary Conversion
         public static string DecimalToBinary(int decimalNumber)
         {
+            /*
+            --LOGIC
+
+            The logic is simple, find the remainder of the number and append the remainder
+            to a string, then divide the number by 2. Keep on doing this until the number is
+            greater than 0. Then return the number.
+
+            Time Complexity: O(n)
+            */
             try
             {
                 string res = string.Empty;
@@ -214,6 +270,14 @@ namespace Assignment_2
         // Question 6: Find Minimum in Rotated Sorted Array
         public static int FindMin(int[] nums)
         {
+            /* 
+            --LOGIC
+
+            I know the original question on leetcode says to achieve this in O(log n) time complexity
+            but I'm still working on that logic. For now, I'm just finding the minimum element in the list.
+
+            Time Complexity: O(n)
+            */
             try
             {
 				int min = int.MaxValue;
@@ -234,6 +298,16 @@ namespace Assignment_2
         // Question 7: Palindrome Number
         public static bool IsPalindrome(int x)
         {
+            /*
+            --LOGIC
+
+            Similar logic as the binary number problem, find the remainder and store it in an
+            empty string. Then divide the number by 10. Basically reversing the number
+            . One thing is to store the initial number to a variable for comparison. Then 
+            compare the result and the initial number, return True if matches, else False. 
+
+            Time Complexity: O(n)
+            */
             try
             {
 				if(x==0){
@@ -256,6 +330,15 @@ namespace Assignment_2
         // Question 8: Fibonacci Number
         public static int Fibonacci(int n)
         {
+            /*
+            -- LOGIC
+
+            Used recursion for solving this problem. The program will recursively call the 
+            function to calculate the fibonacci number. When the number is 1, the value of
+            the Fibonacci is returned.
+
+            Time Complexity: O(2^n).
+            */
             try
             {
                 if(n<=1){
